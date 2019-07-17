cb9ab9eb5ef0432e44952867d626cf146604b24a
Eric Adams <foggyeric@gmail.com>
2019-07-17 10:41:14 -0500

    crabsubmitfiles for the runsI have processed I have chosen not to use a config file so I have a record of what was done for each run

---
+++ CRAB SPECIFIC/crabsubmitcfg_LPC_ZDCanalyzer_327560.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+from CRABClient.UserUtilities import config
+
+#for use storing at LPC
+
+#process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring()) #Added 5/13
+
+config = config()
+config.General.requestName = 'rereco_PbPb2018_AOD_MinBias2_327560_ZDC'
+config.General.transferLogs = True
+config.General.transferOutputs = True
+config.section_('JobType')
+config.JobType.outputFiles = ['rereco_PbPb2018_AOD_MinBias2_327560_RPDZDC.root']
+config.JobType.pyCfgParams = ['noprint']
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'rerecoForCRABpythonfileforModernProduction.py' #MUST BE IN SAME FOLER OR GIVE FULL PATH
+config.Data.inputDataset = '/HIMinimumBias2/HIRun2018A-04Apr2019-v1/AOD' 
+config.Data.splitting = 'FileBased' #LumiBased
+config.Data.unitsPerJob = 20 #20
+config.Data.publication = False
+lumi_mask='Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt' #include this in the same directory as the crab submit
+config.Data.outLFNDirBase = '/store/user/eadams' #I believe this is correct
+config.Data.runRange = '327560'
+config.Site.storageSite = 'T3_US_FNALLPC'
+#'T3_US_UMD'
